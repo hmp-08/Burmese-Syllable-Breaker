406c847bc638e27bb0e7d5d87b8d85c3a54b896e
hmp-08 <110389819+hmp-08@users.noreply.github.com>
2023-06-21 12:59:07 +0630

    Add files via upload

---
+++ SyllableSegmenter.py
@@ -0,0 +1,27 @@
+import re
+
+class SyllableSegmenter:
+    def __init__(self):
+        pass
+
+    def sylseg(self, input_text):
+        input_text = "Ò" + input_text
+        input_text = re.sub("([a-zA-Z]([a-z]+))", "", input_text)
+        input_text = re.sub("([0-9])", r"Ò\1", input_text)
+        input_text = re.sub("([၀-၉])", r"Ò\1", input_text)
+        input_text = re.sub("([\s])", r"Ò\1", input_text)
+        input_text = re.sub("([၊။()!@#$%^&*_=+|\\]×\\/-])", r"Ò\1", input_text)
+        input_text = re.sub("့်", "့်", input_text)
+        input_text = re.sub("([က-ဪဿ၌-၏])", r"Ò\1", input_text)
+        input_text = re.sub("([~])", r"Ò\1", input_text)
+        input_text = re.sub("Ò([က-အ])([္်])", r"\1\2", input_text)
+        input_text = re.sub("([္])Ò([က-အ])", r"\1\2", input_text)
+        input_text = re.sub("([~])", r"Ò\1", input_text)
+        input_text = re.sub("Ò([က-အ])([့်])", r"\1\2", input_text)
+        input_text = re.sub("([Ò]+)", "Ò", input_text)
+        input_text = re.split("Ò", input_text)
+        syl = ' '.join([str(elem) for elem in input_text])
+        syl = re.sub("\s+", " ", syl)
+        syl = re.sub("^\s", "", syl)
+        return syl
+
